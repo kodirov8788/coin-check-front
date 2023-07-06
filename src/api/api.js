2fbe507ea00e6dab5ca2a0270b86b6c0f8d0f90a
kodirov8788 <kodirov8788@gmail.com>
2023-07-06 13:34:39 +0500

    dfsd

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 
-// https://debt-project.onrender.com/
-const instance = axios.create({
-    baseURL: 'http://localhost:5000'
-});
+// https://coin-project.onrender.com
 // const instance = axios.create({
-//     baseURL: 'https://debt-project.onrender.com'
+//     baseURL: 'http://localhost:5000'
 // });
+const instance = axios.create({
+    baseURL: 'https://coin-project.onrender.com'
+});
 
 export default instance
 
