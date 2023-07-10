9e70d2ba343b64f037110c6e7946d021aa393b85
kodirov8788 <kodirov8788@gmail.com>
2023-07-10 06:17:36 +0500

    fixed sign up

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 
-const instance = axios.create({
-    baseURL: 'http://localhost:5000'
-});
 // const instance = axios.create({
-//     baseURL: 'https://coin-project.onrender.com'
+//     baseURL: 'http://localhost:5000'
 // });
+const instance = axios.create({
+    baseURL: 'https://coin-project.onrender.com'
+});
 
 export default instance
 
