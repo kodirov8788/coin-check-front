17442fc4089a3babf5996424a2a5d8a42a763d93
kodirov8788 <kodirov8788@gmail.com>
2023-08-06 15:41:25 +0500

    asdasd

--- src/pages/statistics/Staticchild.js
+++ src/pages/statistics/Staticchild.js
@@ -4,7 +4,10 @@ import Staticsingle from './staticSingle/Staticsingle';
 
 function Staticchild({ arraydata, inx }) {
 
-    // console.log("staticchild :", arraydata)
+    // console.log("staticchild :", arraydata.user)
+
+
+
     return (
         <div className='static_child'>
 
@@ -12,8 +15,6 @@ function Staticchild({ arraydata, inx }) {
                 <h1>{inx + 1}</h1>
                 <h2> To'plangan coin : <span>{arraydata?.allCoin}</span></h2>
                 <h2><a href={`tel:${arraydata?.user[0].number}`}>{arraydata?.user[0].number}</a></h2>
-
-
             </div>
             {arraydata?.user.map((arr, index) => (
                 <Staticsingle key={index} userdata={arr} />
--- src/pages/statistics/Statistics.js
+++ src/pages/statistics/Statistics.js
@@ -9,8 +9,9 @@ import Staticchild from './Staticchild';
 function Statistics() {
     const [total, setTotal] = useState()
     const [Data, setData] = useState([]);
+    const [selectData, setSelectData] = useState("");
     const [arraylist, setArraylist] = useState([]);
-    // console.log(arraylist)
+
     const { isLoading, setIsLoading, sensor, setSensor } = useContext(AuthContext);
     // console.log("data :", Data)
 
@@ -69,6 +70,8 @@ function Statistics() {
 
 
     const Arraylist = () => {
+
+        const [data, setData] = useState([])
         const newArr = arraylist.map(user => {
             const box = []
             user.forEach(ar => box.push(ar.coin))
@@ -76,12 +79,27 @@ function Statistics() {
 
             return { allCoin, user }
         })
-
         const arrayData = newArr.sort((a, b) => b.allCoin - a.allCoin)
-        // console.log("array data", arrayData)
-        return arrayData.map((array, index) => (
+
+        useEffect(() => {
+            if (newArr) {
+
+                const filteredData = arrayData.filter(use => use.allCoin > +selectData && use.allCoin < (+selectData + 100));
+
+                if (selectData === "") {
+                    setData(arrayData);
+                } else {
+                    setData(filteredData);
+
+                }
+            }
+        }, [selectData]);
+
+        // newArr.forEach(use => box.push(use.allCoin > Number(selectData) || use.coin < (Number(selectData) + 100)))
+        return data.map((array, index) => (
             <Staticchild key={index} arraydata={array} inx={index} />
         ))
+
 
     }
 
@@ -89,6 +107,16 @@ function Statistics() {
         <div className="client">
             <Link to={"/login"}>Loginga qaytish</Link>
             <h1>Statistika</h1>
+            <select onChange={(e) => setSelectData(e.target.value)}>
+                <option value="0">miqdorni tanglang</option>
+                <option value="50">50 dan tepasi</option>
+                <option value="100">100 dan tepasi</option>
+                <option value="200">200 dan tepasi</option>
+                <option value="300">300 dan tepasi</option>
+                <option value="400">400 dan tepasi</option>
+                <option value="500">500 dan tepasi</option>
+                <option value="600">600 dan tepasi</option>
+            </select>
             <Arraylist />
         </div >
     )
