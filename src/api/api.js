3c5f6fc429c0f679936ce7addf6b4de08dfedd67
kodirov8788 <kodirov8788@gmail.com>
2023-08-25 10:19:49 +0500

    sdfsd

--- src/api/api.js
+++ src/api/api.js
@@ -8,9 +8,9 @@ import axios from "axios";
 //     baseURL: 'https://coin-project.onrender.com'
 // });
 const Api = axios.create({
-    // baseURL: 'https://coin21.uz'
+    baseURL: 'https://coin21.uz'
     // baseURL: 'http://localhost:5000'
-    baseURL: 'https://coin-project.onrender.com'
+    // baseURL: 'https://coin-project.onrender.com'
 
 });
 
